b1c4ffe0697a962490805d52b981eee5508841e4
Lutz <79272468+LutzEmbeddedTec@users.noreply.github.com>
2022-08-01 17:58:26 +0200

    Update switch_off.py

--- switch_off.py
+++ switch_off.py
@@ -5,7 +5,7 @@ from signal import pause
 def shutdown():
     check_call(['sudo', 'poweroff'])
 
-shutdown_btn = Button(17, hold_time=2)
+shutdown_btn = Button(3, hold_time=2)
 shutdown_btn.when_held = shutdown
 
 pause()
